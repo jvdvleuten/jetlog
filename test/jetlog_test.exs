e8978dd3802e65d8e95cb0ab6e1edf6ff678aa5c
Johan van der Vleuten <jvdvleuten@gmail.com>
2020-05-02 01:20:27 +0200

    Initial testing

--- test/jetlog_test.exs
+++ test/jetlog_test.exs
@@ -1,8 +1,12 @@
 defmodule JetlogTest do
-  use ExUnit.Case
+  alias Jetlog.Logbook.Entry.Event
+  use ExUnit.Case, async: true
   doctest Jetlog
 
-  test "greets the world" do
-    assert Jetlog.hello() == :world
+  test "changing flightnumber" do
+    state = %Jetlog.Logbook.Entry{}
+    event = %Event.FlightnumberChanged{flightnumber: "KL123", flightnumber_v: 0}
+    new_state = Event.FlightnumberChanged.apply(event |> Event.create(), state)
+    assert new_state.flightnumber == "KL1234"
   end
 end
