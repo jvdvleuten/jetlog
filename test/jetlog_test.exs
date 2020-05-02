7a2b305e2512859605debcb566cf2725ad9b8aad
Johan van der Vleuten <jvdvleuten@gmail.com>
2020-05-02 19:56:56 +0200

    fix test

--- test/jetlog_test.exs
+++ test/jetlog_test.exs
@@ -7,6 +7,6 @@ defmodule JetlogTest do
     state = %Jetlog.Logbook.Entry{}
     event = %Event.FlightnumberChanged{flightnumber: "KL123", flightnumber_v: 0}
     new_state = Event.FlightnumberChanged.apply(event |> Event.create(), state)
-    assert new_state.flightnumber == "KL1234"
+    assert new_state.flightnumber == "KL123"
   end
 end
